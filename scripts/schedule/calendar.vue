33d809b96f07ce7629068b132e97eeb580f02716
Pierre Tasci <ptasci67@gmail.com>
2017-09-10 07:32:43 -0700

    Added posts to the calendar vue

--- scripts/schedule/calendar.vue
+++ scripts/schedule/calendar.vue
@@ -2,7 +2,9 @@
   .calendar
     template(v-for='day in window_diff')
       .day 
-        .month {{dateFor(day)}}
+        .month 
+          .day-of-week {{dayOfWeekFor(day)}}
+          .day-of-month {{dateFor(day)}}
         .posts(v-for='post in postsFor(day)')
           .post {{timeForPost(post)}}
     button(@click='loadMore') Load more...
@@ -20,9 +22,10 @@ export default {
       schedules: window.augustus.schedules,
       posts: window.augustus.posts.sort((a, b) => 
         moment(a.post_time_utc).valueOf() - moment(b.post_time_utc).valueOf()),
+      // Must store as an epoch timestamp to allow for change updates.
       window_start: moment(today).subtract(today.day() - 0 + 1, 'days')
         .valueOf(),
-      // Exclusive range.
+      // Must store as an epoch timestamp to allow for change updates.
       window_end: moment(today)
         .subtract(today.day() - 0 + 1, 'days')
         .add(7, 'days')
@@ -37,8 +40,8 @@ export default {
         moment(p.post_time_utc).isSameOrBefore(moment(this.window_end)));
     },
     window_diff: function() {
-      return moment.duration(
-        moment(this.window_end).diff(moment(this.window_start))).asDays();
+      console.log(moment(this.window_end).diff(moment(this.window_start), 'days'));
+      return moment(this.window_end).diff(moment(this.window_start), 'days');
     }
   },
 
@@ -46,6 +49,11 @@ export default {
     dateFor: function(dayOffset) {
       const day = moment(this.window_start).add(dayOffset, 'days');
       return day.format('MMMM DD');
+    },
+
+    dayOfWeekFor: function(dayOffset) {
+      const day = moment(this.window_start).add(dayOffset, 'days');
+      return day.format('dddd');
     },
 
     postsFor: function(dayOffset) {
@@ -75,11 +83,15 @@ export default {
   flex-wrap: wrap
 
   .month
-    height: 2rem
+    height: 3rem
     display: flex
+    flex-direction: column
     align-items: center
     justify-content: center
     border-bottom: 1px solid #666666
+
+    .day-of-week
+      font-weight: bold
 
   .day
     height: 300px
